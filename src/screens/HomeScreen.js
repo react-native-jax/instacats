5236bfc079d9f980a83e13c2a56bf3d37c41cdee
Gabriel Reis <gabriel.oreis@gmail.com>
2018-11-11 21:59:45 -0500

    Convert HomeScreen to use FlatList

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,28 +1,53 @@
 import React from 'react';
-import { ScrollView, StyleSheet, SafeAreaView } from 'react-native';
+import { FlatList, StyleSheet, SafeAreaView } from 'react-native';
 import PostCard from '../components/PostCard';
 
 class HomeScreen extends React.Component {
   render() {
-    const post = {
-      imageUrl: 'https://loremflickr.com/320/320',
-      description: 'Cute cat!!!',
-      author: {
-        username: 'joe',
-        avatarUrl: 'https://loremflickr.com/50/50/person',
+    const posts = [
+      {
+        key: '1',
+        imageUrl: 'https://loremflickr.com/320/320',
+        description: 'Cute cat!!!',
+        author: {
+          username: 'joe',
+          avatarUrl: 'https://loremflickr.com/50/50/person',
+        },
       },
-    };
+      {
+        key: '2',
+        imageUrl: 'https://loremflickr.com/320/320',
+        description: 'Cute cat!!!',
+        author: {
+          username: 'jane',
+          avatarUrl: 'https://loremflickr.com/50/50/person',
+        },
+      },
+      {
+        key: '3',
+        imageUrl: 'https://loremflickr.com/320/320',
+        description: 'Cute cat!!!',
+        author: {
+          username: 'jack',
+          avatarUrl: 'https://loremflickr.com/50/50/person',
+        },
+      },
+    ];
 
     return (
-      <ScrollView style={styles.container}>
-        <SafeAreaView>
-          <PostCard post={post} />
-          <PostCard post={post} />
-          <PostCard post={post} />
-        </SafeAreaView>
-      </ScrollView>
+      <FlatList
+        ListHeaderComponent={SafeAreaView}
+        ListFooterComponent={SafeAreaView}
+        data={posts}
+        style={styles.container}
+        renderItem={this.renderItem}
+      />
     );
   }
+
+  renderItem = ({ item }) => {
+    return <PostCard post={item} />;
+  };
 }
 
 const styles = StyleSheet.create({
