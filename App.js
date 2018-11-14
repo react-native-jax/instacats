3087a4a653cd2d1077be3a62c2216872d4dd548c
Gabriel Reis <gabriel.oreis@gmail.com>
2018-11-14 09:26:11 -0500

    Display multiple posts

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native';
+
+const PostCard = ({ post }) => {
+  const { username, avatarUrl, imageUrl, description } = post;
+  return (
+    <View>
+      <View style={styles.userHeader}>
+        <Image source={{ uri: avatarUrl }} style={styles.avatar} />
+        <Text style={styles.username}>{username}</Text>
+      </View>
+      <Image source={{ uri: imageUrl }} style={[styles.box]} />
+      <Text style={styles.description}>
+        <Text style={styles.username}>{username} </Text>
+        {description}
+      </Text>
+    </View>
+  );
+};
 
 export default class App extends React.Component {
   render() {
+    const post = {
+      username: 'joe',
+      avatarUrl: 'https://loremflickr.com/320/240/me?lock=1',
+      imageUrl: 'https://loremflickr.com/320/240?lock=42',
+      description: 'Cute cat!!!',
+    };
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <SafeAreaView>
+          <PostCard post={post} />
+          <PostCard post={post} />
+          <PostCard post={post} />
+        </SafeAreaView>
+      </ScrollView>
     );
   }
 }
@@ -14,8 +49,27 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
+  },
+  box: {
+    width: '100%',
+    aspectRatio: 1,
+    backgroundColor: '#ccc',
+  },
+  description: {
+    margin: 10,
+  },
+  username: {
+    fontWeight: '700',
+  },
+  avatar: {
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    margin: 10,
+  },
+  userHeader: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
